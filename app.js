dfaeca51fd2bac98b7cb2912df6b6a02c1240ce3
Nesrine <nesrine.laabidi20@gmail.com>
2023-05-14 17:14:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+// const verifyWorkingHours = require('./middlewares/verifyWorkingHours'); 
+const router=require("./route.js")
+// app.use(verifyWorkingHours,router);
+
+app.use(router);
+app.use(express.static(__dirname+"/css"))
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000.');
+  });
